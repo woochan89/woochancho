1594d92ca1e3dbc21fae637a0761400e18d2c40c
woochan89 <woochan89@gmail.com>
2020-06-19 12:15:18 +0900

    6 19

--- 제출/API_chapter_4/API_chapter_4/소스.cpp
+++ 제출/API_chapter_4/API_chapter_4/소스.cpp
@@ -1,16 +1,14 @@
 #include<Windows.h>
 #include<math.h>
 #include<time.h>
+#include<stdio.h>
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 void CALLBACK TimeProc(HWND, UINT, UINT, DWORD);
-
 HINSTANCE g_hInst;//글로벌 인스턴스핸들값
 LPCTSTR lpszClass = TEXT("HelloWorld"); //클래스 명 : 창이름
-TCHAR str[256];
 
 float x = 300;
 float y = 300;
-
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPervlnstance, LPSTR lpszCmdParam, int nCmdShow)
 {
@@ -46,16 +44,12 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPervlnstance, LPSTR lpszCmd
 }
 
 
-
-
-
-
+//공튀기기
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 {
 	HDC hdc;
 	PAINTSTRUCT ps;
-	SYSTEMTIME st;
-	static TCHAR sTime[128];
+
 	switch (iMessage)
 	{
 	case WM_CREATE:
@@ -79,14 +73,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 void CALLBACK TimeProc(HWND hWnd, UINT uMsg, UINT idEvent, DWORD dwTime)
 {
 	static int timeb = time(NULL);
-	//static int angle = (timeb % 360) + 1;
-	static int angle = 30;
-	int tmpNum;
+	static int angle = (timeb % 360) + 1;
+	float radian;
 
 	if (((int)x <= 100 || (int)x >= 500) && (angle == 90 || angle == 270))
-	{
 		angle += 180;
-	}
 	else if ((int)x <= 100 || (int)x >= 500)
 	{
 		if (angle >= 270)
@@ -100,9 +91,7 @@ void CALLBACK TimeProc(HWND hWnd, UINT uMsg, UINT idEvent, DWORD dwTime)
 	}
 
 	if (((int)y <= 100 || (int)y >= 500) && (angle == 0 || angle == 180))
-	{
 		angle += 180;
-	}
 	else if ((int)y <= 100 || (int)y >= 500)
 	{
 		if (angle >= 270)
@@ -115,58 +104,51 @@ void CALLBACK TimeProc(HWND hWnd, UINT uMsg, UINT idEvent, DWORD dwTime)
 			angle = -angle;
 	}
 
-
-
 	if (angle > 360)
 		angle -= 360;
 	else if (angle < 0)
 		angle += 360;
 
+	radian = (angle + 270) * 3.14 / 180;
 
-	float tmp;
-
-	tmp = ((double)angle + 270) * 3.14 / 180;
-
-	x = x + cos(tmp) * 1;
-	y = y + sin(tmp) * 1;
-
-
-	//x = x + cos(angle) * 1;
-	//y = y + sin(angle) * 1;
-
+	x = x + cos(radian);
+	y = y + sin(radian);
 
 	InvalidateRect(hWnd, NULL, TRUE);
 }
 
 
 
-//
-//
-//
+//시계 그리기
 //LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 //{
 //	HDC hdc;
 //	PAINTSTRUCT ps;
 //	SYSTEMTIME st;
-//	static TCHAR sTime[128];
+//	TCHAR sTime[20];
 //	int Circle_R = 100;
 //	int Circle_X = 200;
 //	int Circle_Y = 200;
 //	char text[3];
-//	int tmphour = 0;
+//	int tmphour;
+//
+//	GetLocalTime(&st);
+//	sprintf(sTime, "%d:%d:%d", st.wHour, st.wMinute, st.wSecond);
 //
 //	switch (iMessage)
 //	{
 //	case WM_CREATE:
-//		SetTimer(hWnd, 1, 1000, TimeProc);//초
-//		SendMessage(hWnd, 0, 1, 0);
+//		SetTimer(hWnd, 1, 1000, NULL);//초
+//		SendMessage(hWnd, WM_TIMER, 1, 0);
 //		return 0;
+//	case WM_TIMER:
+//		GetLocalTime(&st);
+//		sprintf(sTime, "%d:%d:%d", st.wHour, st.wMinute, st.wSecond);
+//		InvalidateRect(hWnd, NULL, TRUE);
 //	case WM_PAINT:
 //		hdc = BeginPaint(hWnd, &ps);
 //		//원그리기
-//
 //		Ellipse(hdc, Circle_X - Circle_R, Circle_Y - Circle_R + 6, Circle_X + Circle_R, Circle_Y + Circle_R + 6);
-//
 //
 //		//시간그리기
 //
@@ -187,38 +169,28 @@ void CALLBACK TimeProc(HWND hWnd, UINT uMsg, UINT idEvent, DWORD dwTime)
 //			}
 //		}
 //
-//
-//		//for (int i = 1; i <= 12; i++)
-//		//{
-//		//	SetTextAlign(hdc, TA_CENTER);
-//		//	if (i < 10)
-//		//		sprintf(text, "%d%d", 0, i);
-//		//	else
-//		//		sprintf(text, "%d", i);
-//		//	TextOut(hdc, Circle_X + cos(i * 30 * 3.14 / 180)*(Circle_R - 15), Circle_Y + sin(i * 30 * 3.14 / 180)*(Circle_R - 15), TEXT(text), 2);
-//		//}
-//
 //		//초침 그리기
 //		for (int i = 1; i <= 70; i++)
 //		{
-//			SetPixel(hdc, Circle_X + cos(ct->tm_sec / 60 * 3.14 / 180)*i, Circle_Y + sin(ct->tm_sec / 60 * 3.14 / 180)*i, RGB(0, 0, 0));
+//			SetPixel(hdc, Circle_X + cos((st.wSecond * 6 - 90) * 3.14 / 180)*i, Circle_Y + sin((st.wSecond * 6 - 90) * 3.14 / 180)*i, RGB(0, 0, 0));
 //		}
-//
 //		//분침 그리기
 //		for (int i = 1; i <= 60; i++)
 //		{
-//			SetPixel(hdc, Circle_X + cos(ct->tm_min / 60 * 3.14 / 180)*i, Circle_Y + sin(ct->tm_min / 60 * 3.14 / 180)*i, RGB(255, 0, 0));
+//			SetPixel(hdc, Circle_X + cos((st.wMinute * 6 - 90) * 3.14 / 180)*i, Circle_Y + sin((st.wMinute * 6 - 90) * 3.14 / 180)*i, RGB(255, 0, 0));
 //		}
-//
 //		//시침 그리기
+//		if (st.wHour > 12)
+//			tmphour = st.wHour - 12;
+//		else
+//			tmphour = st.wHour;
 //		for (int i = 1; i <= 50; i++)
 //		{
-//			if (ct->tm_hour > 12)
-//				tmphour = ct->tm_hour - 12;
-//			SetPixel(hdc, Circle_X + cos(tmphour / 12 * 3.14 / 180)*i, Circle_Y + sin(tmphour / 12 * 3.14 / 180)*i, RGB(0, 255, 0));
+//			SetPixel(hdc, Circle_X + cos((tmphour * 30 - 90) * 3.14 / 180)*i, Circle_Y + sin((tmphour * 30 - 90) * 3.14 / 180)*i, RGB(0, 255, 0));
 //		}
 //
-//
+//		//시간 그리기
+//		TextOut(hdc, Circle_X, Circle_Y + Circle_R + 50, sTime, lstrlen(sTime));
 //
 //		EndPaint(hWnd, &ps);
 //		return 0;
@@ -228,28 +200,4 @@ void CALLBACK TimeProc(HWND hWnd, UINT uMsg, UINT idEvent, DWORD dwTime)
 //		return 0;
 //	}
 //	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
-//}
-//
-//void CALLBACK TimeProc(HWND hWnd, UINT uMsg, UINT idEvent, DWORD dwTime)
-//{
-//
-//
-//	(ct->tm_sec)++;
-//	if (ct->tm_sec >= 60)
-//	{
-//		ct->tm_sec = 0;
-//		(ct->tm_min)++;
-//		if (ct->tm_min >= 60)
-//		{
-//			(ct->tm_hour)++;
-//			if (ct->tm_hour > 12)
-//				ct->tm_hour = 1;
-//		}
-//	}
-//
-//
-//
-//
-//
-//	InvalidateRect(hWnd, NULL, TRUE);
 //}
